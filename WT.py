180e686e12ae47a928c8486474535c0679affa7e
surya-narayanan <surya21@stanford.edu>
2024-03-27 00:44:13 +0000

    d

--- WT.py
+++ WT.py
@@ -12,6 +12,10 @@ from PIL import Image
 from insightface.app import FaceAnalysis
 from pipeline_stable_diffusion_xl_instantid import StableDiffusionXLInstantIDPipeline, draw_kps
 
+import pathlib
+import random
+num_images_per_prompt = 100
+
 # prepare 'antelopev2' under ./models
 app = FaceAnalysis(name='antelopev2', root='./', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(640, 640))
@@ -35,9 +39,14 @@ pipe.load_ip_adapter_instantid(face_adapter)
 # %%
 # load an image
 for original in pathlib.Path('/home/user/InstantID/Originals').iterdir():
-    orignal_filename = original.split('/')[-1].split('.')[0]
-    face_image = load_image(original)
+    
+    print(original)
 
+    orignal_filename = str(original).split('/')[-1].split('.')[0]
+    face_image = load_image(str(original))
+    face_image = face_image.convert('RGB')
+    face_image = face_image.resize((512, 512))
+    
     # prepare face emb
     face_info = app.get(cv2.cvtColor(np.array(face_image), cv2.COLOR_RGB2BGR))
     face_info = sorted(face_info, key=lambda x:(x['bbox'][2]-x['bbox'][0])*x['bbox'][3]-x['bbox'][1])[-1] # only use the maximum face
@@ -46,9 +55,6 @@ for original in pathlib.Path('/home/user/InstantID/Originals').iterdir():
 
     pipe.set_ip_adapter_scale(0.8)
 
-
-
-    # %%
     prompts = [
         'Photo of a woman skiing in Switzerland.',
         'Photo of a woman paragliding in Austria',
@@ -61,22 +67,19 @@ for original in pathlib.Path('/home/user/InstantID/Originals').iterdir():
         'Photo of a woman in the statue of liberty in New York'
         ]
 
-
     negative_prompt = "(lowres, low quality, worst quality:1.2), (text:1.2), watermark, (frame:1.2), deformed, ugly, deformed eyes, blur, out of focus, blurry, deformed cat, deformed, photo, anthropomorphic cat, monochrome, pet collar, gun, weapon, blue, 3d, drones, drone, buildings in background, green, anime, cartoon, graphic, (blur, blurry, bokeh), text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured"
 
-    # %%
-    import pathlib
-    import random
-    num_images_per_prompt = 1
-
     for prompt in prompts:
+        print(prompt)
+
         path = pathlib.Path(f'/home/user/InstantID/Generated/{prompt}/{orignal_filename}')
         path.mkdir(parents=True, exist_ok=True)
         random.seed(9001)
         
-        prompt += ' cinematic still a photo of a man . emotional, harmonious, vignette, 4k epic detailed, shot on kodak, 35mm photo, sharp focus, high budget, cinemascope, moody, epic, gorgeous, film grain, grainy'
+        prompt += ' cinematic still a photo of a woman. emotional, harmonious, vignette, 4k epic detailed, shot on kodak, 35mm photo, sharp focus, high budget, cinemascope, moody, epic, gorgeous, film grain, grainy'
         
-        for _ in range(num_images_per_prompt):    
+        for asd in range(num_images_per_prompt):    
+
             random_int = random.randint(1, num_images_per_prompt)
 
             image = pipe(prompt=prompt, 
@@ -92,3 +95,5 @@ for original in pathlib.Path('/home/user/InstantID/Originals').iterdir():
                                                                     
 
 
+
+# %%
